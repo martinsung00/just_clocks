38a21993f83cae371e25ba8ede4d26156f4f4a64
Martin <martinsung00@gmail.com>
2021-06-05 01:03:49 -0700

    Update Readme images. Account for no .env file created edge case.

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 
 import app from "./routes";
 
-const port =
+const envSetup =
   process.env.MODE === "production"
     ? process.env.PROD_PORT
     : process.env.DEV_PORT;
+
+const port = !envSetup ? 3000 : envSetup;
 
 app.listen(port, () => console.log(`Listening on ${port}`));
